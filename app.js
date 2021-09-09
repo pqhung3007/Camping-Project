5a0382c75027d9c2b87d423fcf819fcc08d144fa
pqhung3007 <hungpham307@gmail.com>
2021-09-09 12:52:11 +0700

    ready to deploy

--- app.js
+++ app.js
@@ -17,10 +17,13 @@ const User = require('./models/user')
 
 const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds')
-const reviewRoutes = require('./routes/reviews')
+const reviewRoutes = require('./routes/reviews');
+const MongoStore = require('connect-mongo');
+// const MongoDBStore = require("connect-mongo")(session)
+// const dbUrl = process.env.DB_URL
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
 
-
-mongoose.connect('mongodb://localhost:27017/yelp-camp', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false });
 
 const db = mongoose.connection
 db.on("error", console.error.bind(console, "connection error:"))
@@ -38,8 +41,20 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(express.static(path.join(__dirname, 'public')))
 
+const secret = process.env.SECRET || 'thisisasecret'
+const store = new MongoStore({
+    mongoUrl: dbUrl,
+    secret,
+    touchAfter: 24 * 60 * 60
+})
+
+store.on('error', function (e) {
+    console.log('SESSION ERROR', e);
+})
+
 const sessionConfig = {
-    secret: 'thisisasecret',
+    store,
+    SpeechRecognitionResult,
     resave: false,
     saveUninitialized: true,
     cookie: {
